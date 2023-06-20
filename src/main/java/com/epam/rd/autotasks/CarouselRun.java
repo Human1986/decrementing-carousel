da8a948e47092dbd7283ed51644dd2d0dda7f9d7
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-06-20 14:34:27 +0000

    2

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,30 @@
 package com.epam.rd.autotasks;
 
-public class CarouselRun {
+public class CarouselRun extends DecrementingCarousel {
+
+     protected int position = 0;
+
+    public CarouselRun(int capacity) {
+        super(capacity);
+    }
+
 
     public int next() {
-       throw new UnsupportedOperationException();
-    }
+            if (isFinished())
+                return -1;
+            else {
+                while (carousel[position %= carousel.length] <= 0) {
+                    position++;
+                }
+            }
+            return carousel[position++]--;
+        }
 
-    public boolean isFinished() {
-        throw new UnsupportedOperationException();
-    }
+        public boolean isFinished () {
+            for (int el : carousel)
+                if (el > 0)
+                    return false;
+            return true;
+        }
 
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,33 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+     private final int capacity;
+    public int[] carousel;
+    int index;
+    boolean isRun;
+    {
+        index = 0;
+        isRun = false;
+    }
 
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        this.carousel = new int[capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (element > 0 && index < capacity && !isRun) {
+            carousel[index++] = element;
+            return true;
+        }
+        return false;
     }
 
-    public CarouselRun run(){
-       throw new UnsupportedOperationException();
+    public CarouselRun run() {
+        if (!isRun) {
+            isRun = true;
+            return new CarouselRun(capacity);
+        }
+        return null;
     }
 }
